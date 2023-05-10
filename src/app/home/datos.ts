0951a497671937125fae89e0b3de64af2bb9d0f9
LucasGetsCode <lucas.pujia@gmail.com>
2023-05-10 11:37:14 -0300

    Agregado manejarDatos y datos agregados

--- src/app/home/datos.ts
+++ src/app/home/datos.ts
@@ -1,5 +1,13 @@
-export const TASKS = [
+import {Datos} from "./manejarDatos"
+export const TASKS: Datos[] = [
     {
-        
+        id: 1,
+        text: "Manzana roja",
+        title: "Roberto"
+    },
+    {
+        id: 2,
+        text: "Remolacha verde",
+        title: "Jorgito"
     }
 ]
---
+++ src/app/home/manejarDatos.ts
@@ -0,0 +1,5 @@
+export interface Datos {
+    id?: number, // al iniciarse no tiene un id, por eso el ?
+    text: string,
+    title: string
+}
