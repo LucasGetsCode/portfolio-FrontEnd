d915fb93922a816d41f5b49a99d3042b05b0068e
LucasGetsCode <lucas.pujia@gmail.com>
2023-05-10 00:07:00 -0300

    Probando funciones en el info

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css']
 })
-export class InfoComponent {
+export class InfoComponent implements OnInit {
+  @Input () text: string = "";
+  @Input () title: string = "Ramón";
+  @Input () color: string = "";
 
+
+  contructor() {}
+
+  ngOnInit(): void{
+  }
+
+  onClick(){
+    console.log("Click de " + this.title)
+  }
 }
